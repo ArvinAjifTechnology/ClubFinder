e31c030496bbf99a2c692c8d82bb0fd891818e95
ArvinAjifTechology <arvinajif3@gmail.com>
2023-09-28 10:20:35 +0700

    Add Component

--- src/script/view/main.js
+++ src/script/view/main.js
@@ -1,13 +1,15 @@
-import DataSource from "../data/data-source";
+import "../component/club-list.js";
+import "../component/search-bar.js";
+import DataSource from "../data/data-source.js";
+
 const main = () => {
-  const searchElement = document.querySelector("#searchElement");
-  const buttonSearchElement = document.querySelector("#searchButtonElement");
+  const searchElement = document.querySelector("search-bar");
   const clubListElement = document.querySelector("#clubList");
 
   const onButtonSearchClicked = async () => {
     try {
-      const results = await DataSource.searchClub(searchElement.value);
-      renderResult(results);
+      const result = await DataSource.searchClub(searchElement.value);
+      renderResult(result);
     } catch (message) {
       fallbackResult(message);
     }
@@ -15,27 +17,30 @@ const main = () => {
 
   const renderResult = (results) => {
     clubListElement.innerHTML = "";
+
     results.forEach((club) => {
       const { name, fanArt, description } = club;
-
       const clubElement = document.createElement("div");
       clubElement.setAttribute("class", "club");
 
-      clubElement.innerHTML = `<img class="fan-art-club" src="  ${fanArt}  " alt="Fan Art"> 
-          <div class="club-info"> 
-          <h2>  ${name}  </h2> 
-          <p>  ${description}  </p> 
-          </div>`;
+      clubElement.innerHTML = `
+        <img class="fan-art-club" src="${fanArt}" alt="Fan Art">
+        <div class="club-info">
+          <h2>${name}</h2>
+          <p>${description}</p>
+        </div>
+      `;
+
       clubListElement.appendChild(clubElement);
     });
   };
 
   const fallbackResult = (message) => {
     clubListElement.innerHTML = "";
-    clubListElement.innerHTML += `<h2 class="placeholder">  ${message}  </h2>`;
+    clubListElement.innerHTML += `<h2 class="placeholder">${message}</h2>`;
   };
 
-  buttonSearchElement.addEventListener("click", onButtonSearchClicked);
+  searchElement.clickEvent = onButtonSearchClicked;
 };
 
 export default main;
